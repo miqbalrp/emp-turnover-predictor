878605e96c676166ae7e9c41a73ef48709fafda2
Iqbal Rahmadhan <miqbalrp@gmail.com>
2024-02-29 00:16:00 +0700

    start on streamlit

---
+++ src/streamlit.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import requests
+from PIL import Image
+
+st.title("Employer Turnover Prediction")
+st.subheader("Want to know how likely your employee to turnover and why? :cry: :thinking_face: :cry: :thinking_face: :cry: :thinking_face: :cry: :thinking_face:")
